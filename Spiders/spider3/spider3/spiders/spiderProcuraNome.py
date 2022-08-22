530b2d50d6c2bc97448bd88d435854be8f8ce98f
hannanhunny01 <hannanhoney5000@gmail.com>
2022-08-22 01:50:54 -0300

    spider 3 funcionando

--- Spiders/spider3/spider3/spiders/spiderProcuraNome.py
+++ Spiders/spider3/spider3/spiders/spiderProcuraNome.py
@@ -9,6 +9,7 @@ from sqlalchemy import true
 from scrapy.selector import Selector
 import numpy as np
 from pymongo import MongoClient
+from time import sleep
 
 
 
@@ -39,7 +40,7 @@ class SpiderprocuranomeSpider(scrapy.Spider):
     def start_requests(self):
         yield SeleniumRequest(
             url="https://security.cebraspe.org.br/PAS_21/Consulta1Chamada2Semestre_582AECCD/default.aspx",
-            wait_time=10,
+            wait_time=5,
             screenshot=true,
             callback=self.parse
         )
@@ -49,14 +50,14 @@ class SpiderprocuranomeSpider(scrapy.Spider):
     
     def parse(self, response):
 
-
+       
         
      #  img = response.meta['screenshot']
 
     #  with open('screenshot.png','wb') as f:
      #   f.write(img)
 
-       
+      #  pessoas_found=[]
         count=0
         cluster = MongoClient("mongodb+srv://cebraspe-tracker:cebraspe-tracker@cluster0.sa63e.mongodb.net/?retryWrites=true&w=majority")
 
@@ -67,34 +68,44 @@ class SpiderprocuranomeSpider(scrapy.Spider):
         array_nomes =  list(collection_users.find({},{'nome':1,'_id':0}))
         arrays_of_names = np.array(array_nomes)
         
-        for i in range(total_users):
+        for i in range(total_users+1):
             driver= response.meta['driver']
             searchinput = driver.find_element("xpath",'//*[@id="txtNome"]')
 
             nome = arrays_of_names[count]['nome']
-
+            driver.find_element("xpath",'//*[@id="txtNome"]').clear()
       #    searchinput.send_keys('Matheus Rodrigues da Silva')
             searchinput.send_keys(nome)
           
             driver.find_element("xpath",'//*[@id="btnBuscar"]').click()
+            
         #searchinput.send_keys(Keys.ENTER)
-            driver.save_screenshot('search.png')
+            driver.save_screenshot(str(count)+'search.png')
 
 
             html = driver.page_source
             count=count+1
-            
+            sleep(10)
             
             response_obj = Selector(text=html)
+
+            sleep(10)
+
+            #found = response_obj.xpath("//*[@id='GridView1']/tbody/tr[2]/td[2]/text()").get().strip()
+          #  pessoas_found.append(found)
         
             yield{
-            'item':response_obj.xpath("//*[@id='GridView1']/tbody/tr[2]/td[2]/text()").get().strip()
+           ' item':response_obj.xpath("//*[@id='GridView1']/tbody/tr[2]/td[2]/text()").get().strip()
             }
-
+            sleep(10)
+            
+            
             
             driver.back()
             
             
+        driver.quit()  
+       
 
             
 
